6b55c90c53fe83ea8a28ad5fb0b6909109080f6c
TofekHamodii <43014320+TofekHamodii@users.noreply.github.com>
2018-10-19 19:01:54 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -2,8 +2,8 @@ const Discord = require('discord.js');
 const client = new Discord.Client();
 const fs = require('fs');
 const suck = JSON.parse(fs.readFileSync('./suck.json', 'utf8'));
-const prefix = "!";
-// By M7MD
+const prefix = "#";
+// By Simple
 client.on("message", message => {
     fs.writeFile('./suck.json', JSON.stringify(suck));
 });

808a8b8a242ae38f399cd0169b3e6158cd8d9ae6
TofekHamodii <43014320+TofekHamodii@users.noreply.github.com>
2018-10-19 20:18:04 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,40 +1,78 @@
-const Discord = require('discord.js');
-const client = new Discord.Client();
-const fs = require('fs');
-const suck = JSON.parse(fs.readFileSync('./suck.json', 'utf8'));
-const prefix = "#";
-// By Simple
-client.on("message", message => {
-    fs.writeFile('./suck.json', JSON.stringify(suck));
-});
-client.on('ready', () => {
-    setInterval(function(){
-        client.guilds.forEach(g => {
-            if (suck[g.id]) {
-                if (suck[g.id].role) {
-                    var role = g.roles.get(suck[g.id].role);
-                    if (role) {
-                        role.edit({color : "RANDOM"});
-                    };
-                };
-            };
-        });
-    }, 1500);
-});
-client.on("message", message => {
-    if (!message.content.startsWith(prefix)) return;
-    if (message.author.bot) return;
-    if (message.channel.type !== "text") return message.reply("This Command Is Only Allowed In Servers");
-    var args = message.content.split(" ");
-    var command = args[0].slice(prefix.length);
-    switch(command) {
-        case "rainbow" :
-        if (!message.member.hasPermission("MANAGE_ROLES")) return message.reply("no no");
-        message.guild.createRole({name : "rainbow", color : "RANDOM"}).then(r => {
-            r.edit({color : "RANDOm"});
-            suck[message.guild.id] = {role : r.id};
-        });
-    };
-});
+client.on('message', message => {
+
+              if(!message.channel.guild) return;
+
+    var prefix = "#";
+
+    if(message.content.startsWith('bc')) {
+
+    if(!message.channel.guild) return message.channel.send('**هذا الأمر فقط للسيرفرات**').then(m => m.delete(5000));
+
+  if(!message.member.hasPermission('ADMINISTRATOR')) return      message.channel.send('**للأسف لا تمتلك صلاحية** `ADMINISTRATOR`' );
+
+    let args = message.content.split(" ").join(" ").slice(2 + prefix.length);
+
+
+    let request = `Requested By ${message.author.username}`;
+
+    if (!args) return message.reply('**اكتب شي لي ارسال البرودكاست**');message.channel.send(`**هل أنت متأكد من إرسالك البرودكاست؟ \nمحتوى البرودكاست:** \` ${args}\``).then(msg => {
+
+    msg.react('✅')
+
+    .then(() => msg.react('❌'))
+
+    .then(() =>msg.react('✅'))
+
+    let reaction1Filter = (reaction, user) => reaction.emoji.name === '✅' && user.id === message.author.id;
+
+    let reaction2Filter = (reaction, user) => reaction.emoji.name === '❌' && user.id === message.author.id;
+
+       let reaction1 = msg.createReactionCollector(reaction1Filter, { time: 12000 });
+
+    let reaction2 = msg.createReactionCollector(reaction2Filter, { time: 12000 });
+
+    reaction1.on("collect", r => {
+
+    message.channel.send(`☑ | Done ... The Broadcast Message Has Been Sent For ${message.guild.members.size} Members`).then(m => m.delete(5000));
+
+    message.guild.members.forEach(m => {
+
+    var bc = new
+
+       Discord.RichEmbed()
+
+       .setColor('RANDOM')
+
+         .setTitle('لي دخول السيرفر اضغط هنا')
+       
+       .setURL('حط رابط سيرفرك')
+
+       .addField('Server', message.guild.name)
+
+       .addField('Sender', message.author.username)
+
+       .addField('Message', args)
+
+    m.send({ embed: bc })
+
+    msg.delete();
+
+    })
+
+    })
+
+    reaction2.on("collect", r => {
+
+    message.channel.send(`**Broadcast Canceled.**`).then(m => m.delete(5000));
+
+    msg.delete();
+
+    })
+
+    })
+
+    }
+
+    });  
 
 client.login(process.env.BOT_TOKEN);

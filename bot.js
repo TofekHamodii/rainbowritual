6546176b2164a3ee780a807c70863463e483b72c
Simple <43014320+TofekHamodii@users.noreply.github.com>
2018-11-24 12:49:36 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,13 +1,57 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
-const prefix = "-";
-client.on('ready', () => {
-    console.log(`Logged in as ${client.user.tag}!`);
-    console.log(`in ${client.guilds.size} servers `)
-    console.log(`[AlphaCodes] ${client.users.size}`)
-    client.user.setStatus("online")   
+
+
+client.on('ready', function() {
+    console.log(`i am ready ${client.user.username}`);
 });
 
+
+
+
+
+
+
+
+
+
+
+const developers = ["507978288250093568","",""]
+const adminprefix = "*";
+client.on('message', message => {
+    var argresult = message.content.split(` `).slice(1).join(' ');
+      if (!developers.includes(message.author.id)) return;
+      
+  if (message.content.startsWith(adminprefix + 'ply')) {
+    client.user.setGame(argresult);
+      message.channel.send(`**Now Playig   ${argresult}**`)
+  } else 
+     if (message.content === (adminprefix + "leaveserver")) {
+    message.guild.leave();        
+  } else  
+  if (message.content.startsWith(adminprefix + 'wat')) {
+  client.user.setActivity(argresult, {type:'WATCHING'});
+      message.channel.send(`**Now Watching   ${argresult}**`)
+  } else 
+  if (message.content.startsWith(adminprefix + 'lis')) {
+  client.user.setActivity(argresult , {type:'LISTENING'});
+      message.channel.send(`**Now Listening   ${argresult}**`)
+  } else 
+  if (message.content.startsWith(adminprefix + 'stream')) {
+    client.user.setGame(argresult, "https://www.twitch.tv/idk");
+      message.channel.send(`**Now Streaming   ${argresult}**`)
+  }
+  if (message.content.startsWith(adminprefix + 'setname')) {
+  client.user.setUsername(argresult).then
+      message.channel.send(`Changing The Name To ..**${argresult}** `)
+} else
+if (message.content.startsWith(adminprefix + 'setavatar')) {
+  client.user.setAvatar(argresult);
+    message.channel.send(`Changing The Avatar To :**${argresult}** `);
+}
+});
+
+var prefix = "-"
 client.on("message", message => {
 
             if (message.content.startsWith(prefix + "bc")) {
@@ -22,29 +66,5 @@ client.on("message", message => {
 };     
 });
 
-const adminprefix = "c";
-const devs = ['507978288250093568'];
-client.on('message', message => {
-  var argresult = message.content.split(`).slice(1).join(' ');
-    if (!devs.includes(message.author.id)) return;
-    
-if (message.content.startsWith(adminprefix + 'setG')) {
-  client.user.setGame(argresult);
-    message.channel.sendMessage(${argresult} تم تغيير بلاينق البوت إلى )
-} else 
-  if (message.content.startsWith(adminprefix + 'setN')) {
-client.user.setUsername(argresult).then
-    message.channel.sendMessage(${argresult} : تم تغيير أسم البوت إلى)
-return message.reply("**لا يمكنك تغيير الاسم يجب عليك الانتظآر لمدة ساعتين . **");
-} else
-  if (message.content.startsWith(adminprefix + 'setA')) {
-client.user.setAvatar(argresult);
-  message.channel.sendMessage(${argresult} : تم تغير صورة البوت);
-      } else     
-if (message.content.startsWith(adminprefix + 'setT')) {
-  client.user.setGame(argresult, "https://www.twitch.tv/idk");
-    message.channel.sendMessage(تم تغيير تويتش البوت إلى  ${argresult}`)
-}
-});
 
 client.login(process.env.BOT_TOKEN);
